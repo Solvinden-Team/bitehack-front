3e1bc29579cc0acd92811665f8ad04a12f36a118
Paweł Taborowski <peantab@gmail.com>
2020-01-12 00:15:08 +0100

    Visual tweaks.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 import Room from './Room.js'
-import {GridList, GridListTile, Paper} from '@material-ui/core';
-import { styled } from '@material-ui/core/styles';
-
-
-const RoomTile = styled(Paper)({
-    elevation: 24,
-});
+import {GridList, GridListTile} from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
 
 const peopleInsideStr = "People inside: ";
 
@@ -27,16 +24,17 @@ class App extends React.Component {
             .then(data => {
                 let roomsFromJson = [];
                 for (let i = 0; i < data.length; i++) {
-                    roomsFromJson.push(Room({id: i, name: data[i].room, count: data[i].peopleCount, warningLevel: 0}));
+                    roomsFromJson.push(Room({id: i, name: data[i].room, count: data[i].peopleCount, description: data[i].roomDescription, warningLevel: 0}));
                 }
                 this.setState({ rooms: roomsFromJson })})
             .catch(_ => this.setState({ rooms: [
-                    Room({id: 0, name: "kuchniaOffline1", count: 7, warningLevel: 0}),
-                    Room({id: 1, name: "kuchniaOffline2", count: 3, warningLevel: 0})
+                    Room({id: 0, name: "Kuchnia off-line 1", count: 7, description: "Ala ma kota", warningLevel: 0}),
+                    Room({id: 1, name: "Kuchnia off-line 2", count: 3, description: "Ola ma psa", warningLevel: 0})
                 ] }));
     }
 
     render() {
+
         return (
             <div className="App">
                 <GridList cellHeight={'auto'} className="Room-grid" cols={4}>
@@ -44,12 +42,20 @@ class App extends React.Component {
                         <h1>Chwytliwa Nazwa</h1>
                     </GridListTile>
                     {this.state.rooms.map(tile => (
-                        <RoomTile key={tile.id} className="RoomTile">
-                            <h2 align={'center'}>{tile.name}</h2>
-                            <h3>{peopleInsideStr}{tile.peopleCount}</h3>
+                        <Card className="RoomTile" variant="outlined">
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    {tile.name}
+                                </Typography>
+                                <Typography className="RoomDescription" color="textSecondary">
+                                    {tile.description}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {peopleInsideStr}{tile.peopleCount}
+                                </Typography>
                             {/*<h4>{tile.warningLevel}</h4>*/}
-                            <text>{tile.description}</text>
-                        </RoomTile>
+                            </CardContent>
+                        </Card>
                     ))}
                 </GridList>
             </div>
